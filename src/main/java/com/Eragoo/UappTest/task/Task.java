fac9588a3c2db656575f37c0598496bd5af84010
Yevhenii Kukhol <yevhenii.kukhol@gmail.com>
2020-09-07 20:16:27 +0300

    change "order" column name to "priority"

--- src/main/java/com/Eragoo/UappTest/task/Task.java
+++ src/main/java/com/Eragoo/UappTest/task/Task.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.Instant;
 
@@ -12,6 +14,7 @@ import java.time.Instant;
 @Setter
 public class Task {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
