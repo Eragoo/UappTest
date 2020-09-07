b301e954039ca7a191ffb7c9e63ba2650b5f005b
Yevhenii Kukhol <yevhenii.kukhol@gmail.com>
2020-09-07 17:33:05 +0300

    init commit

---
+++ src/main/java/com/Eragoo/UappTest/UappTestApplication.java
@@ -0,0 +1,13 @@
+package com.Eragoo.UappTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UappTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UappTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Eragoo/UappTest/UappTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Eragoo.UappTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UappTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
