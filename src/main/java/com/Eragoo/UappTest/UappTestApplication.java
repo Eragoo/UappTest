7d0021fc1e48c4b1769aff3b4d9dab067c9414b3
Yevhenii Kukhol <yevhenii.kukhol@gmail.com>
2020-09-07 18:02:20 +0300

    swagger added

--- src/main/java/com/Eragoo/UappTest/UappTestApplication.java
+++ src/main/java/com/Eragoo/UappTest/UappTestApplication.java
@@ -2,12 +2,12 @@ package com.Eragoo.UappTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class UappTestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UappTestApplication.class, args);
 	}
-
 }
